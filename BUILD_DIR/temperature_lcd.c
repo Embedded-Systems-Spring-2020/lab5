b6fbec6e4b7f7db51294e42e0627def6a683a50a
Garrett Smith <ggasmithh@users.noreply.github.com>
2020-02-24 15:54:58 -0600

    Added starting point for application

---
+++ BUILD_DIR/temperature_lcd.c
@@ -0,0 +1,21 @@
+#include "pic24_all.h"
+#include <p33EP512GP806.h>
+#include "pic24_ports_config.h"
+
+#include <esos.h>
+#include <esos_task.h>
+#include "esos_comm.h"
+#include "esos_f14ui.h"
+#include "esos_pic24.h"
+#include "esos_sensor.h"
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void user_init(void){
+    //Your code goes here.
+    //Good luck!
+}
+
+
